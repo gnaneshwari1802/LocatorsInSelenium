1b410b9bdc2fda1a4a23c5289e9f1cc413e98a2d
Gnaneshwari Mahimaluru <gnaneshwari.m2009@gmail.com>
2025-03-15 11:07:06 +0530

    Update locators.java

--- locators.java
+++ locators.java
@@ -44,3 +44,41 @@ public class locators {
 	 driver.close(); */
 }
 }
+
+
+package day22; 
+import org.openqa.selenium.chrome.ChromeDriver; 
+import org.openqa.selenium.By; 
+import org.openqa.selenium.WebDriver; 
+public class Locators Demo { 
+public static void main(String[] args) { 
+WebDriver driver=new ChromeDriver(); 
+driver.get("https://demo.opencart.com/"); 
+driver.manage().window().maximize(); 
+//name 
+//driver.findElement(By.name("search")).sendKeys("Mac"); 
+//id 
+//boolean logoDisplaystatus=driver.findElement(By.id("logo")).is Displayed(); 
+//System.out.println(logoDisplaystatus); 
+//linktext & partialLinkText 
+//driver.findElement(By.linkText("Tablets")).click(); 
+//driver.findElement(By.partialLinkText("Table")).click(); 
+//name 
+//driver.findElement(By.name("search")).sendKeys("Mac"); 
+//id 
+//boolean logoDisplaystatus=driver.findElement(By.id("logo")).isDisplayed(); 
+//System.out.println(logoDisplaystatus); 
+//linktext & partialLinkText 
+//driver.findElement(By.linkText("Tablets")).click(); 
+//driver.findElement(By.partialLinkText("Table")).click(); 
+//classname 
+//List<WebElement> headerLinks=driver.findElements(By.className("list-inline-item")); 
+//System.out.println("total number of header links: "+headerLinks.size()); 
+//tagname 
+//List<WebElement> links=driver.findElements (By.tagName("a")); 
+//System.out.println("total nmber of links: "+links.size()); 
+List<WebElement> images=driver.findElements (By.tagName("img")); 
+System.out.println("total number of images: "+ images.size());
+}
+}
+	
